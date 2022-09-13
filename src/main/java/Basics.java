09b2e78e74f0d8a2870805c681c33bc437a6537b
ileuxali <92287421+ileuxali@users.noreply.github.com>
2022-09-13 02:22:17 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -40,9 +40,6 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
         System.out.println("Hello World!");
 
 
